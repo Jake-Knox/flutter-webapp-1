2c29c75b35457708d5c62aa8497caaf2e3073e1f
jake-knox- <jakeknox11@hotmail.co.uk>
2023-05-25 16:13:04 +0100

    adding files

---
+++ lib/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to the Home Page',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/second');
+              },
+              child: Text('Go to Second Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
